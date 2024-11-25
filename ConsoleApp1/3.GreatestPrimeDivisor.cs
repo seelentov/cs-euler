8337493d50af61a889635a067231496d31571803
vladisluw <komkov222111@gmail.com>
2024-11-26 03:53:50 +0500

    Add 3,4

---
+++ ConsoleApp1/3.GreatestPrimeDivisor.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ConsoleApp1;
+
+public class GreatestPrimeDivisor
+{
+    private static int Solution(int num)
+    {
+        int res = 1;
+
+        for (int i = 2; i < num; i++)
+        {
+            bool skip = false;
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    skip = true;
+                }
+            }
+
+            if (skip)
+            {
+                continue;
+            }
+
+            if (num % i == 0 && i > res)
+            {
+                res = i;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ ConsoleApp1/4.GreatestProductPalindrome.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace ConsoleApp1;
+
+public class GreatestProductPalindrome
+{
+    private static int Solution()
+    {
+        int res = 0;
+
+        int start = 100;
+        int end = 999;
+
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                int multipleInt = i * j;
+
+                string multiple = multipleInt.ToString();
+
+                int length = multiple.Length;
+
+                if (length % 2 != 0)
+                {
+                    continue;
+                }
+
+                int center = length / 2;
+
+                string left = multiple[..center];
+                string right = multiple[center..];
+
+                char[] charArray = right.ToCharArray();
+                Array.Reverse(charArray);
+                string rightReverse = new(charArray);
+
+                if (left == rightReverse)
+                {
+                    res = multipleInt;
+                }
+
+            }
+        }
+
+        return res;
+    }
+}
